886fcb31cd5be1b5d38b12edfd51d4128cdfa485
onachoo <123869430+onachoo@users.noreply.github.com>
2023-10-10 10:09:20 +0200

    subirArray

---
+++ UD2/ExemploArray1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ExemploArray1 {
+  
+    public static void main(String[] args) {
+        
+        int [] arrayEnteiros = new int[7];
+
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i = 0; i < arrayEnteiros.length; i++) {
+            System.out.println("Introduce un valor enteiro");
+            arrayEnteiros[i] = entrada.nextInt();
+        }
+
+        arrayEnteiros[3] = 0; 
+
+        System.out.println(arrayEnteiros[0]);
+
+        for (int enteiro : arrayEnteiros) {  /* ACCEDO AO VALOR DIRECTAMENTE */
+            System.out.println(enteiro);
+        }
+        
+        for(i = 0; i < 7; i++){  /* ACCEDO AO VALOR EMPREGANDO O ÍNDICE */
+            System.out.println("O número na posición "+i+" é "+arrayEnteiros[i]);
+        }
+
+        }
+    }
