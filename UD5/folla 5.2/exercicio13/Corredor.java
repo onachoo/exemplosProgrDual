163820facf0a4547a3093c51209b091ba1f252f8
onachoo <123869430+onachoo@users.noreply.github.com>
2024-05-02 22:28:18 +0200

    Add files via upload

---
+++ UD5/folla 5.2/exercicio13/Corredor.java
@@ -0,0 +1,13 @@
+package exercicio13;
+
+public class Corredor {
+
+    int dorsal;
+    double tempo;
+
+    public Corredor(int dorsa, double temp){
+        dorsal = dorsa;
+        tempo = temp;
+    }
+
+}
